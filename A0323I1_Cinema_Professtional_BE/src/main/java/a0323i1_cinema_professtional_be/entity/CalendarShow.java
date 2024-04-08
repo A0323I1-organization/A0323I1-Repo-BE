694f1b489e7877c909b5eaf59b5e3ea8a3eaa77a
LeQuangAnh2002 <quangngocbqn@gmail.com>
2024-04-08 16:19:55 +0700

    A0323I1-BE

---
+++ A0323I1_Cinema_Professtional_BE/src/main/java/a0323i1_cinema_professtional_be/entity/CalendarShow.java
@@ -0,0 +1,35 @@
+package a0323i1_cinema_professtional_be.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "calendar_show")
+public class CalendarShow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "calendar_show_id")
+    private int calendarShowId;
+    @Column(name = "calendar_show_detail")
+    private String calendarShowDetail;
+    @ManyToOne
+    @JoinColumn(name = "movie_id",referencedColumnName = "movie_id")
+    private Movie movie;
+    @ManyToOne
+    @JoinColumn(name = "room_id",referencedColumnName = "room_id")
+    private Room room;
+    @ManyToOne
+    @JoinColumn(name = "show_time_id",referencedColumnName = "show_time_id")
+    private ShowTime showTime;
+
+    @OneToMany(mappedBy = "calendarShow")
+    private List<Ticket> ticketList;
+}
---
+++ A0323I1_Cinema_Professtional_BE/src/test/java/a0323i1_cinema_professtional_be/A0323I1CinemaProfesstionalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package a0323i1_cinema_professtional_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A0323I1CinemaProfesstionalBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
