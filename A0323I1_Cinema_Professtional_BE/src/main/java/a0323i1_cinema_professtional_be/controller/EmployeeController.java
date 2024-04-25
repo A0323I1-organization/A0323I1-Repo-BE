59c9f8f08ce479709305523944d9014c8ca1b327
thanhngan <thanhngan19803@gmail.com>
2024-04-25 16:06:44 +0700

    Ngan 25/4

--- A0323I1_Cinema_Professtional_BE/src/main/java/a0323i1_cinema_professtional_be/controller/EmployeeController.java
+++ A0323I1_Cinema_Professtional_BE/src/main/java/a0323i1_cinema_professtional_be/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package a0323i1_cinema_professtional_be.controller;
 
 import a0323i1_cinema_professtional_be.entity.Employee;
 import a0323i1_cinema_professtional_be.exception.ApiResponse;
+import a0323i1_cinema_professtional_be.exception.ResourceNotFoundException;
 import a0323i1_cinema_professtional_be.service.employee.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,9 +12,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.NoHandlerFoundException;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,12 +29,11 @@ public class EmployeeController {
     public ResponseEntity<Page<Employee>> findAll(@PageableDefault(page = 0, size = 5) Pageable pageable,
                                                   @RequestParam(required = false, defaultValue = "") String fullname,
                                                   @RequestParam(required = false, defaultValue = "") String phone,
-                                                  @RequestParam(required=false,defaultValue = "EmployeeId") String sort) {
+                                                  @RequestParam(required=false,defaultValue = "employeeId") String sort) {
         Sort sort1 = Sort.by(Sort.Direction.ASC, sort);
         Pageable pageableWithSort = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort1);
         Page<Employee> list = employeeService.findAll("%" + fullname + "%", "%" + phone + "%", pageableWithSort);
-        System.out.println(list);
-        return new ResponseEntity<Page<Employee>>(list, HttpStatus.OK);
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @DeleteMapping("/{ids}")
@@ -43,7 +42,7 @@ public class EmployeeController {
         List<String> result = new ArrayList<>();
         String mes = null;
         for (int i = 0; i < number.length; i++) {
-            if (Integer.parseInt(number[i]) <= 0) {
+            if (Integer.parseInt(number[i]) <= 0 ) {
                 return new ResponseEntity<>(new ApiResponse("Invalid id", false), HttpStatus.BAD_REQUEST);
             } else {
                 employeeService.deleteEmployee(Integer.parseInt(number[i]));
@@ -54,6 +53,14 @@ public class EmployeeController {
             mes="Employee with ID " + ids + " has been deleted successfully.";
         }
         return new ResponseEntity<>(new ApiResponse(mes, true), HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> findById(@PathVariable("id")int id){
+        Employee employeeFindId = employeeService.findById(id);
+        if(employeeFindId== null){
+            throw new ResourceNotFoundException("Employee", " Id ", id);
+        }
+        return new ResponseEntity<>(employeeFindId, HttpStatus.OK);
     }
 }
 
--- A0323I1_Cinema_Professtional_BE/src/main/java/a0323i1_cinema_professtional_be/exception/ResourceNotFoundException.java
+++ A0323I1_Cinema_Professtional_BE/src/main/java/a0323i1_cinema_professtional_be/exception/ResourceNotFoundException.java
@@ -1,9 +1,9 @@
 package a0323i1_cinema_professtional_be.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
-    String resourceName;
-    String fieldName;
-    long fieldValue;
+    final String resourceName;
+    final String fieldName;
+    final long fieldValue;
 
     public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
         super(String.format("%s not found with %s : %d", resourceName,fieldName,fieldValue));
