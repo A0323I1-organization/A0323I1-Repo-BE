95fe22e89b7e489c00555e862f9c52a73829a9ca
trieutran025 <65442760+trieutran025@users.noreply.github.com>
2024-04-26 19:11:57 +0700

    Update EmployeeRepository.java

--- A0323I1_Cinema_Professtional_BE/src/main/java/a0323i1_cinema_professtional_be/repository/employee/EmployeeRepository.java
+++ A0323I1_Cinema_Professtional_BE/src/main/java/a0323i1_cinema_professtional_be/repository/employee/EmployeeRepository.java
@@ -1,35 +1,6 @@
 package a0323i1_cinema_professtional_be.repository.employee;
 
 
-import a0323i1_cinema_professtional_be.entity.Employee;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
-import org.springframework.data.domain.Page;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import org.springframework.stereotype.Repository;
-
-import java.util.Date;
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import javax.transaction.Transactional;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    @Query("SELECT e FROM Employee e JOIN e.account a WHERE a.status = 1 AND e.fullname LIKE :fullname AND e.phone LIKE :phone")
-    Page<Employee> findAll(@Param("fullname")String fullname, @Param("phone")String phone, Pageable pageable);
-    @Modifying
-    @Transactional
-    @Query("DELETE FROM Employee e WHERE e.id = :id")
-    void deleteEmployeeById(@Param("id") int id);
-
-    @Query("SELECT e FROM Employee e WHERE e.id = :id")
-    Employee findEmployeeById(@Param("id") int id);
-
-
-
-
 }
