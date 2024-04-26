f2e4b4f75b42d35f15b4f6e5ddf0573abdd39e9c
trieutran025 <65442760+trieutran025@users.noreply.github.com>
2024-04-26 19:12:41 +0700

    Update EmployeeService.java

--- A0323I1_Cinema_Professtional_BE/src/main/java/a0323i1_cinema_professtional_be/service/employee/EmployeeService.java
+++ A0323I1_Cinema_Professtional_BE/src/main/java/a0323i1_cinema_professtional_be/service/employee/EmployeeService.java
@@ -7,19 +7,4 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 @Service
 public interface EmployeeService {
-    void create(Employee employee);
-    void update(Employee employee);
-    Employee findById(int id);
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
-
-public interface EmployeeService {
-    Page<Employee> findAll(String fullname, String phone,Pageable pageable);
-    void deleteEmployee(int id);
-
-
-
 }
